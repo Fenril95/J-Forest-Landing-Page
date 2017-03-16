6b7eedda665b5ceee5d1c5f98e122e5592ea9d77
Petro Kryvolap <Petro.Kryvolap95@gmail.com>
2017-03-16 23:02:41 +0200

    new coomit for portfolio

---
+++ README.md
@@ -0,0 +1,1 @@
+# J-Forest-Landing-Page
---
+++ js/script.js
@@ -0,0 +1,122 @@
+var socialMedia = {
+  facebook : 'http://facebook.com/profile.php?id=100004690685995',
+  twitter: 'http://youtube.com/watch?v=tvEQQFOq4ls',
+  instagram: 'http://youtube.com/watch?v=tvEQQFOq4ls'
+};
+
+var socialList = function() {
+  var output = '<div class="links">',
+  myList = document.querySelectorAll('.links');
+
+  for (var key in arguments[0]) {
+    output += '<a href="' + socialMedia[key] + '">' +
+      '<img src="img/' + key + '.png" alt="icon for '+key+'">' +
+      '</a>';
+  }
+  output += '</div>';
+  
+  for (var i = myList.length - 1; i >= 0; i--) {
+    myList[i].innerHTML = output;
+  };
+}(socialMedia);
+
+
+
+
+// ---------
+(function() {
+
+  // Selecting our node
+  var myNode = document.querySelector('.gallery');
+
+  myNode.addEventListener("click", function(e) {
+
+    if (e.target.tagName === 'IMG') {
+
+      var myOverlay = document.createElement('div');
+      myOverlay.id = 'overlay';
+      document.body.appendChild(myOverlay);
+
+      // set up overlay styles
+      myOverlay.style.position = 'absolute';
+      myOverlay.style.top = 0;
+      myOverlay.style.backgroundColor = 'rgba(0,0,0,0.7)';
+      myOverlay.style.cursor = 'pointer';
+
+      // resize and position overlay
+      myOverlay.style.width = window.innerWidth + 'px';
+      myOverlay.style.height = window.innerHeight + 'px';
+      myOverlay.style.top = window.pageYOffset + 'px';
+      myOverlay.style.left = window.pageXOffset + 'px';
+
+      // Create image element
+      var imageSrc = e.target.src;
+      var largeImage = document.createElement('img');
+      largeImage.id = 'largeImage';
+      largeImage.src = imageSrc;
+      largeImage.style.display = 'block';
+      largeImage.style.position = 'absolute';
+
+      // wait until the image has loaded
+      largeImage.addEventListener("load", function() {
+
+        // Resize if taller
+        if (this.height > window.innerHeight) {
+          this.ratio = window.innerHeight / this.height;
+          this.height = this.height * this.ratio;
+          this.width = this.width * this.ratio;
+        }
+
+        // Resize if wider
+        if (this.width > window.innerWidth) {
+          this.ratio = window.innerWidth / this.width;
+          this.height = this.height * this.ratio;
+          this.width = this.width * this.ratio;
+        }
+
+        centerImage(this);
+        myOverlay.appendChild(largeImage);
+
+      }, false); // image has loaded
+
+      largeImage.addEventListener('click', function() {
+        if (myOverlay) {
+          window.removeEventListener('resize', window, false);
+          window.removeEventListener('scroll', window, false);
+          myOverlay.parentNode.removeChild(myOverlay);
+        }
+      }, false);
+
+      window.addEventListener('scroll', function() {
+        if (myOverlay) {
+          myOverlay.style.top = window.pageYOffset + 'px';
+          myOverlay.style.left = window.pageXOffset + 'px';
+        }
+      }, false);
+
+      window.addEventListener('resize', function() {
+        if (myOverlay) {
+          myOverlay.style.width = window.innerWidth + 'px';
+          myOverlay.style.height = window.innerHeight + 'px';
+          myOverlay.style.top = window.pageYOffset + 'px';
+          myOverlay.style.left = window.pageXOffset + 'px';
+
+          centerImage(largeImage);
+        }
+      }, false);
+
+    } // target is an image
+
+  }, false); // image is clicked
+
+  function centerImage(theImage) {
+    var myDifX = (window.innerWidth - theImage.width)/2;
+    var myDifY = (window.innerHeight - theImage.height)/2;
+
+    theImage.style.top = myDifY + 'px';
+    theImage.style.left = myDifX + 'px';
+
+    return theImage;
+  }
+
+})();
